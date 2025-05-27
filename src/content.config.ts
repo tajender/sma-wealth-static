797ddf1c9b97835ceaaac4f0612265bbcb369b89
Tajender Pal Singh <tp.singh@emint.co.in>
2025-05-27 15:33:08 +0530

    Added sample code company

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,5 +14,21 @@ const blog = defineCollection({
     heroImage: z.string().optional(),
   }),
 });
+const company = defineCollection({
+  // Load Markdown and MDX files in the `src/content/company/` directory.
+  loader: glob({ base: "./src/content/company", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
 
-export const collections = { blog };
+
+export const collections = { blog, company };
+
+
